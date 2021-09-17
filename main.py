f3290abf98f99af93b0a529ca39fdc4cb7bfba86
estebancabreraa <esteban.cabrera.a@gmail.com>
2021-09-16 18:29:39 -0600

    example

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import requests
 
-r = requests.post(url='https://arnulfo190699.github.io/data/test.json', json={"nombre": "Jarx",  "edad":22})
-print(r.status_code)
-
 r = requests.get(url='https://arnulfo190699.github.io/data/test.json')
 try:
     data = r.json()
-    for object in data['users']:
+    for object in data:
         print(object)
 except ValueError:
     print("Response content is not valid JSON")
