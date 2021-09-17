eb8fbe469eefb87606263a62ecff2dcd8debdeac
estebancabreraa <esteban.cabrera.a@gmail.com>
2021-09-16 18:14:40 -0600

    change test json

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import requests
 r = requests.get(url='https://arnulfo190699.github.io/data/test.json')
 try:
     data = r.json()
+    for object in data:
+        print(object)
 except ValueError:
     print("Response content is not valid JSON")
+
+
