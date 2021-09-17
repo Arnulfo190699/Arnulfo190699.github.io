1d972b44eab38a84c607d9edf3b2f007473216f7
estebancabreraa <esteban.cabrera.a@gmail.com>
2021-09-16 18:17:53 -0600

    demo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 r = requests.get(url='https://arnulfo190699.github.io/data/test.json')
 try:
     data = r.json()
-    for object in data:
+    for object in data['cars']['Nissan']:
         print(object)
 except ValueError:
     print("Response content is not valid JSON")
